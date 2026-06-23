db2382230094035e8b028283e814772d18a142ff
Kaustubh Salunke <kaustubh.v.salunke@gmail.com>
2026-06-23 12:55:09 +0200

    request_body_prac

--- fast_api_prac.py
+++ fast_api_prac.py
@@ -51,3 +51,41 @@ def get_all_models():
 @app.get("/models/{model_name}")
 def which_model(model_name : Models):
     return {"model":model_name}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# @app.get("/item")
+# async def read_item(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]
+
+
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name : str
+    description : str | None = None
+    price : float
+    tax : float | None = None
+
+items = []
+@app.post("/items")
+def create_item(item : Item):
+    items.append(item)
+    return item
+
+@app.get("/items")
+def get_items():
+    return items
+
+
+@app.post("/items/fss")
+async def create_items(item : Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax" : price_with_tax})
+    return item_dict
+
